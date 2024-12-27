90415763f1206f7757b1e2d3f46a5a9851c454f9
abdelhamidsoussi <122819227+abdelhamidsoussi@users.noreply.github.com>
2024-12-27 16:29:36 +0100

    Update database.sql

--- database.sql
+++ database.sql
@@ -19,3 +19,18 @@ CREATE TABLE comptes (
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
     FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE
 );
+INSERT INTO roles (role_name) VALUES
+('Admin'),
+('Etudiant'),
+('Professeur');
+
+INSERT INTO users (nom, prenom, email) VALUES
+('karim', 'yasini', 'jkarimyassin@gmail.com'),
+('said', 'Sfyani', 'saidSfyani@gmail.com'),
+('mohammed', 'jibali', 'mohammedjibali@houtmail.com');
+
+INSERT INTO comptes (user_id, username, password, role_id) VALUES
+(1, 'karim', 'password1', 1),
+(2, 'said', 'password2', 2),
+(3, 'mohammed', 'password3', 3);   
+
